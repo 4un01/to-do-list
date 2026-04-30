aaf5cce813f7b4a493fe71f3bcab876569d4c7a7
Aun <aunh9277@gmail.com>
2026-04-30 22:15:22 +0100

    Fixed empty input that is added to tasks bug

--- main.js
+++ main.js
@@ -1,43 +1,55 @@
 let add = document.getElementById('add');
 
 function addOrRemoveTask(){
-    let taskInput = document.getElementById('taskInput').value;
+    let taskInput = document.getElementById('taskInput');
+    if(taskInput.value === ''){
+        taskInput.placeholder = 'You must input a task';
+        taskInput.classList.add('error');
+        return;
+    }
+    else{
+        taskInput.placeholder = 'Write anything and hit enter to add';
+        taskInput.classList.remove('error');
+
+        let task = document.createElement('div');
+        task.className = 'tasks';
+        
+        let checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.className = 'checkbox';
+
+        let taskValue = document.createElement('span');
+        taskValue.className = 'taskValue'
+        taskValue.textContent = taskInput.value;
+
+        let deleteIcon = document.createElement('img');
+        deleteIcon.className = 'deleteIcon';
+        deleteIcon.src = './images/deleteIcon.svg';
+
+        checkbox.addEventListener('change', () => {
+            if(checkbox.checked){
+            taskValue.style.textDecoration = 'line-through'; 
+            taskValue.style.color = '#696969';
+            taskValue.style.opacity = 0.9; 
+            }
+            else{
+                taskValue.style.textDecoration = 'none';
+                taskValue.style.color = 'white';
+            }
+        });
+
+        document.body.appendChild(task);
+        task.appendChild(checkbox);
+        task.appendChild(taskValue);
+        task.appendChild(deleteIcon);
+
+        deleteIcon.addEventListener('click', () => {
+            document.body.removeChild(task);
+        });
+
+        taskInput.value = '';
+    }
     
-    let task = document.createElement('div');
-    task.className = 'tasks';
-    
-    let checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.className = 'checkbox';
-
-    let taskValue = document.createElement('span');
-    taskValue.className = 'taskValue'
-    taskValue.textContent = taskInput;
-
-    let deleteIcon = document.createElement('img');
-    deleteIcon.className = 'deleteIcon';
-    deleteIcon.src = './images/deleteIcon.svg';
-
-    checkbox.addEventListener('change', () => {
-        if(checkbox.checked){
-          taskValue.style.textDecoration = 'line-through'; 
-          taskValue.style.color = '#696969';
-          taskValue.style.opacity = 0.9; 
-        }
-        else{
-            taskValue.style.textDecoration = 'none';
-        }
-    });
-
-    document.body.appendChild(task);
-    task.appendChild(checkbox);
-    task.appendChild(taskValue);
-    task.appendChild(deleteIcon);
-
-    deleteIcon.addEventListener('click', () => {
-        document.body.removeChild(task);
-    });
-
 };
 
 add.addEventListener('click', addOrRemoveTask);
