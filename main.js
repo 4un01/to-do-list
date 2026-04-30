6e022728350f2387d4d2a0354da6f6339b33a6ea
Aun <aunh9277@gmail.com>
2026-04-30 17:41:07 +0100

    Coded the logic for adding tasks

---
+++ main.js
@@ -0,0 +1,26 @@
+let add = document.getElementById('add');
+
+function addTask(){
+    let taskInput = document.getElementById('taskInput').value;
+    
+    let task = document.createElement('div');
+    task.className = 'tasks';
+    
+    let checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.className = 'checkbox';
+
+    let taskValue = document.createElement('span');
+    taskValue.textContent = taskInput;
+
+    let deleteIcon = document.createElement('img');
+    deleteIcon.className = 'deleteIcon';
+    deleteIcon.src = './images/deleteIcon.svg';
+
+    document.body.appendChild(task);
+    task.appendChild(checkbox);
+    task.appendChild(taskValue);
+    task.appendChild(deleteIcon);
+};
+
+add.addEventListener('click', addTask);
