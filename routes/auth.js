72d9b2b6dc0a9637450805e3b6e1823b2e58a454
4un01 <aunh9277@gmail.com>
2026-06-25 19:09:33 +0100

    Added my route files and set up my authentication route

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,6 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const connectionStr = process.env.MONGO_CONNECTION;
 
 const router = express.Router();
-mongoose.connect(connectionStr)
-    .then(() => {console.log('Connected to MongoDB')})
-    .catch((e) => {console.log(e.message)});
 
 router.post('/login', (req, res) => {});
 router.post('/signup', (req, res) => {});
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const auth = require('./routes/auth');
 
 const app = express();
 const PORT = process.env.PORT;
+const connectionStr = process.env.MONGO_CONNECTION;
 
-app.use('/login', auth);
-app.use('/signup', auth);
+mongoose.connect(connectionStr)
+    .then(() => {console.log('Connected to MongoDB')})
+    .catch((e) => {console.log(e.message)});
+
+app.use(express.text());
+app.use(express.json());    
+app.use('/auth', auth);
 
 app.listen(PORT, () => {console.log(`Server is listening on port: ${PORT}`)});
