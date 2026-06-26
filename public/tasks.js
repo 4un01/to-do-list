4333ce27f72bf432618c75e15afcc170cb91991b
4un01 <aunh9277@gmail.com>
2026-06-26 15:57:44 +0100

    Finished refactoring tasks.js by adding the delete task

--- public/tasks.js
+++ public/tasks.js
@@ -1,55 +1,5 @@
 let add = document.getElementById('add');
 
-function addOrRemoveTask(){
-    let taskInput = document.getElementById('taskInput');
-    if(taskInput.value === ''){
-        taskInput.placeholder = 'You must input a task';
-        taskInput.classList.add('error');
-        return;
-    }
-    else{
-        taskInput.placeholder = 'Write anything and hit enter to add';
-        taskInput.classList.remove('error');
-
-        let task = document.createElement('div');
-        task.className = 'tasks';
-        
-        let checkbox = document.createElement('input');
-        checkbox.type = 'checkbox';
-        checkbox.className = 'checkbox';
-
-        let taskValue = document.createElement('span');
-        taskValue.className = 'taskValue'
-        taskValue.textContent = taskInput.value;
-
-        let deleteIcon = document.createElement('img');
-        deleteIcon.className = 'deleteIcon';
-        deleteIcon.src = './images/deleteIcon.svg';
-
-        checkbox.addEventListener('change', () => {
-            if(checkbox.checked){
-            taskValue.style.textDecoration = 'line-through'; 
-            taskValue.style.color = '#696969';
-            taskValue.style.opacity = 0.9; 
-            }
-            else{
-                taskValue.style.textDecoration = 'none';
-                taskValue.style.color = 'white';
-            }
-        });
-
-        document.body.appendChild(task);
-        task.appendChild(checkbox);
-        task.appendChild(taskValue);
-        task.appendChild(deleteIcon);
-
-        deleteIcon.addEventListener('click', () => { document.body.removeChild(task)});
-
-        taskInput.value = '';
-    }
-    
-}
-
 function addTask(){
     let taskInput = document.getElementById('taskInput');
 
@@ -69,7 +19,16 @@ function addTask(){
         deleteIcon.className = 'deleteIcon';
         deleteIcon.src = './images/deleteIcon.svg';
 
-        checkbox.addEventListener('change', isChecked);
+        checkbox.addEventListener('change', () => isChecked(checkbox, taskValue));
+        deleteIcon.addEventListener('click', () => deleteTask(task));
+
+        document.body.appendChild(task);
+        task.appendChild(checkbox);
+        task.appendChild(taskValue);
+        task.appendChild(deleteIcon);
+
+        taskInput.value = '';
+
     }else{
         return;
     }
@@ -86,7 +45,7 @@ function isInputEmpty(){
     }
 }
 
-function isChecked(){
+function isChecked(checkbox, taskValue){
     if(checkbox.checked){
         taskValue.style.textDecoration = 'line-through'; 
         taskValue.style.color = '#696969';
@@ -97,4 +56,8 @@ function isChecked(){
     }
 }
 
-add.addEventListener('click', addOrRemoveTask);
+function deleteTask(task){
+    document.body.removeChild(task);
+}
+
+add.addEventListener('click', addTask);
