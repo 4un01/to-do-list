af764714ef3950909b4d5d2e4820bfa01531aea0
4un01 <aunh9277@gmail.com>
2026-06-26 17:23:32 +0100

    Wrote the server logic for saving a task to the database

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,5 +1,16 @@
 const User = require('../models/users');
 
-const saveToDb = async (req, res) => {};
+const saveToDb = async (req, res) => {
+    try{
+        const {email, task} = req.body;
+        const user = await User.find({email: email});
+        user.tasks = task;
+        await user.save;
+        res.status(202).send('task added');
+    }catch(e){
+        console.log(e.message);
+        res.status(400).send('failed');
+    }
+};
 
 module.exports = saveToDb;
